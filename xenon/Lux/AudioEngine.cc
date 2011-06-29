ca3f8ace10c90147c527b789d51518263c98cfb4
fenn <fennetic.net>
2011-06-28 18:44:20 -0700

    partial fix. what's going on?

--- xenon/Lux/AudioEngine.cc
+++ xenon/Lux/AudioEngine.cc
@@ -15,11 +15,14 @@ lux::AudioEngine::AudioEngine(std::string const& jack_endpoint_name) :
   std::ostringstream l_ostr,r_ostr;
   l_ostr << jack_endpoint_name << ":in_l";
   r_ostr << jack_endpoint_name << ":in_r";
-//  this->connect_ports("system:capture_1", l_ostr.str());
-//  this->connect_ports("system:capture_2", r_ostr.str());
+
+#ifdef __APPLE__
   this->connect_ports("Soundflowerbed:out1", l_ostr.str());
   this->connect_ports("Soundflowerbed:out2", r_ostr.str());
-
+#else
+  this->connect_ports("system:capture_1", l_ostr.str());
+  this->connect_ports("system:capture_2", r_ostr.str());
+#endif
   // ------------
   // Set up Aubio
   // ------------
